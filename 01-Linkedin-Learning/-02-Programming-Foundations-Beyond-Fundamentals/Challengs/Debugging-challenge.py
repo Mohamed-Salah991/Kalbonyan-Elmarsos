9158422e59516122aa8da351a9b9cd7ee7a93d0a
Mohamed-Salah991 <93313116+Mohamed-Salah991@users.noreply.github.com>
2022-07-03 15:10:40 +0200

    Solve the challenge

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
@@ -1,1 +1,12 @@
+def plant_recommendation(care):
+    if care == 'low':  # if ->  ( == )  not (  = ) 
+        print('aloe')
+    elif care == 'medium':
+        print('pothos')
+    elif care == 'high':    # logical error (unexpected output ) => be careful about small things
+        print('orchid')
 
+
+plant_recommendation('low')  # write correct name of function
+plant_recommendation('medium')
+plant_recommendation('high')
