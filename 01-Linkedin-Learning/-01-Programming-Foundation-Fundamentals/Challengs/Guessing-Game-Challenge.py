2af3fcc7f74a14049eb9e6dc2fe4d0a5a9234b28
Mohamed-Salah991 <93313116+Mohamed-Salah991@users.noreply.github.com>
2022-07-03 14:31:20 +0200

    Solve the challenge

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,8 @@
+guess = input("What's my favorite food? ")  # take a value form user
 
+if guess == "cookies":
+    print("Yep! So amazing!")  # take care about space tab uner the if or else
+else:  # any thing other cookies
+    print("Yuck! That’s not it!")
+
+print("Thanks for playing!")
