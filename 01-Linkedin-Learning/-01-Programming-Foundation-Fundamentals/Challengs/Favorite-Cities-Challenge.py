d12ec66f6cc2a18cc54e98581e4ad28ac879a64b
Mohamed-Salah991 <93313116+Mohamed-Salah991@users.noreply.github.com>
2022-07-03 14:30:33 +0200

    Solve the challenge

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,9 @@
+# Prints out the name of a favorite city
+# def => keyword to create a function , name ->  is a paremeter of this function
+def favorite_city(city_name):
+    print("One of my favorite cities is", city_name)
 
+
+favorite_city("Santa Barbara, California")  # one parameter between " "
+favorite_city("Cario, Egypt")
+favorite_city("Amsterdam, The Netherlands")
