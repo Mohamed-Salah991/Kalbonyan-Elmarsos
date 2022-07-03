b173a4e9beb2b9960ce7b4deff8f10580836d6c5
Mohamed-Salah991 <93313116+Mohamed-Salah991@users.noreply.github.com>
2022-07-03 14:55:01 +0200

    Solve the challenge

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
@@ -1,1 +1,5 @@
-
+fruits = ['apples', 'bananas', 'dragon fruit', 'mangos', 'nectarines', 'pears']
+print('Our fruit selection:')
+i = 1
+for fruit in fruits:
+    print(fruit)  # take care about space tab | fruit => it's each element of this list
