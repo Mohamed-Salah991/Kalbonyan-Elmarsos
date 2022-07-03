1481141edf1cd4a85b9be71efd1b252a292c7165
Mohamed-Salah991 <93313116+Mohamed-Salah991@users.noreply.github.com>
2022-07-03 15:01:52 +0200

    Solve the challenge

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Strings-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Strings-challenge.py
@@ -1,1 +1,6 @@
-
+miles = input('Enter a distance in miles: ')
+miles_float = float(miles)  # ( float ) => decimal numbers
+# kilometers_value = miles_value * 1.609344
+kilometers = miles_float * 1.609344
+print('That value in kilometers is')  # every print in single line (output)
+print(kilometers)
